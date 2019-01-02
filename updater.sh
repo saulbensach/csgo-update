9723e295c2fdbb97b5ccc0e5bfb65a5a73405226
Saúl Bensach <saulcube123@gmail.com>
2019-01-02 15:20:41 +0100

    Initial commit

---
+++ updater.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+rootdir="/home/csgoserver/"
+serverfiles="${rootdir}/serverfiles"
+appid="740"
+steamcmddir="${rootdir}/steamcmd"
+steamuser="anonymous"
+branch="public"
+branchname="public"
+
+fn_appmanifest_info(){
+        appmanifestfile=$(find "${serverfiles}" -type f -name "appmanifest_${appid}.acf")
+        appmanifestfilewc=$(find "${serverfiles}" -type f -name "appmanifest_${appid}.acf" | wc -l)
+}
+
+fn_appmanifest_info
+currentbuild=$(grep buildid "${appmanifestfile}" | tr '[:blank:]"' ' ' | tr -s ' ' | cut -d\  -f3)
+if [ -f "${HOME}/Steam/appcache/appinfo.vdf" ]; then
+        rm -f "${HOME}/Steam/appcache/appinfo.vdf"
+fi
+# Set branch for updateinfo
+
+# Gets availablebuild info
+cd "${steamcmddir}" || exit
+availablebuild=$(./steamcmd.sh +login "${steamuser}" +app_info_update 1 +app_info_print "${appid}" +quit | sed '1,/branches/d' | sed "1,/${branchname}/d" | grep -m 1 buildid | tr -cd '[:digit:]')
+
+if [ "${currentbuild}" != "${availablebuild}" ]; then
+    echo "telling masterserver to update!"
+else
+    echo "no update available"
+fi
