bd159ad8c888480a99be6dcf8158b4d58eea919d
Saúl Bensach <saulcube123@gmail.com>
2019-01-14 16:17:38 +0100

    added manage sh with update before start and fixed updater

--- updater.sh
+++ updater.sh
@@ -21,24 +21,32 @@ do
     if [ -f "${HOME}/Steam/appcache/appinfo.vdf" ]; then
         rm -f "${HOME}/Steam/appcache/appinfo.vdf"
     fi
-    # Set branch for updateinfo
+    # Set branch for updateinfonano
 
     # Gets availablebuild info
     cd "${steamcmddir}" || exit
     availablebuild=$(./steamcmd.sh +login "${steamuser}" +app_info_update 1 +app_info_print "${appid}" +quit | sed '1,/branches/d' | sed "1,/${branchname}/d" | grep -m 1 buildid | tr -cd '[:digit:]')
 
-    if [ "${currentbuild}" != "${availablebuild}" ]; then
-        call=$(curl http://81.202.122.97:4000/master)
-        cd "${rootdir}" || exit
-        ./csgoserver update
-        # hacer case si hay algún tipo de error con update etc blablalba
-        call=$(curl http://81.202.122.97:4000/master/updated)
-        echo "server updated"
-    else
-        echo "no update available"
-        sleep 0.5
+    if ! [ -z $availablebuild ]; then
+        if [ "${currentbuild}" != "${availablebuild}" ]; then
+            l1=$currentbuild | wc -c
+            l2=$availablebuild | wc -c
+            if [ "$l1" = "$l2" ]; then
+                echo "updated detected!"
+                echo "Currentbuild: $currentbuild"
+                echo "Availablebuild: $availablebuild"
+                call=$(curl http://81.202.122.97:4000/master)
+                cd "${rootdir}" || exit
+                ./csgoserver update
+                # hacer case si hay algún tipo de error con update etc blablalba
+                call=$(curl http://81.202.122.97:4000/master/updated)
+                echo "server updated"
+            fi
+        else
+            echo "no update available"
+            sleep 0.5
+        fi
     fi
-
     sleep 7
 done
 
